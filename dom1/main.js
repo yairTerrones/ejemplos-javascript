2c241df42e329397f9ad5310d9c9739d66dca1ad
Daniel Torres <youkiddingd@gmail.com>
2017-02-28 13:00:23 -0600

    Primer ejemplo de Dom.

---
+++ dom1/main.js
@@ -0,0 +1,4 @@
+var cajas = document.getElementsByTagName('div')
+//var cajas = document.getElementsByClassName('div')//para una caja.
+cajas[0].textContent = 'Hola mundo'
+cajas[1].innerHTML = '<b>Hola JavaScript</b>'

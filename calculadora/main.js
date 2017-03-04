38012695c144dd3a0a794dc2c0b21f5a7bd1378b
yairTerrones <yair82010@hotmail.com>
2017-03-03 18:01:35 -0600

    Se agregaron los eventos.

--- calculadora/main.js
+++ calculadora/main.js
@@ -1,28 +1,108 @@
 
-var btnPress = ""
-var anterior =""
-
-function agregaApantalla(){
-    var contenedor = document.getElementById('textoEnPantalla')
-    contenedor.textContent = btnPress + anterior
-    anterior = contenedor.textContent
+function agregaApantalla(unBoton){
+    console.log(unBoton)
+    var contenedor = document.getElementById('textoEnPantalla');
+    contenedor.textContent = contenedor.textContent + unBoton
 }
 
 //Nueve
-btnPress ='9'
-var btnNueve = document.getElementById('nueve')
-btnNueve.onclick = function () {
-    agregaApantalla()
-}
+BtnNueve = document.getElementById('nueve');
+BtnNueve.addEventListener('click', function(){ 
+    agregaApantalla('9');
+});
+
 //Ocho
-btnPress ='8'
-var btnOcho = document.getElementById('ocho')
-btnOcho.addEventListener('click',agregaApantalla)
+BtnOcho = document.getElementById('ocho');
+BtnOcho.addEventListener('click', function(){ 
+    agregaApantalla('8');
+});
+
 //Siete
-btnPress ='7'
-var btnSiete = document.getElementById('siete')
-btnSiete.addEventListener('click',agregaApantalla)
+BtnSiete = document.getElementById('siete');
+BtnSiete.addEventListener('click', function(){ 
+    agregaApantalla('7');
+});
+
+//Seis
+BtnSeis = document.getElementById('seis');
+BtnSeis.addEventListener('click', function(){ 
+    agregaApantalla('6');
+});
+
+//Cinco
+BtnCinco = document.getElementById('cinco');
+BtnCinco.addEventListener('click', function(){ 
+    agregaApantalla('5');
+});
+
+//Cuatro
+BtnCuatro = document.getElementById('cuatro');
+BtnCuatro.addEventListener('click', function(){ 
+    agregaApantalla('4');
+});
+
+//Tres
+BtnTres = document.getElementById('tres');
+BtnTres.addEventListener('click', function(){ 
+    agregaApantalla('3');
+});
+
+//Dos
+BtnDos = document.getElementById('dos');
+BtnDos.addEventListener('click', function(){ 
+    agregaApantalla('2');
+});
+
+//Uno
+BtnUno = document.getElementById('uno');
+BtnUno.addEventListener('click', function(){ 
+    agregaApantalla('1');
+});
+
+//Cero
+BtnCero = document.getElementById('cero');
+BtnCero.addEventListener('click', function(){ 
+    agregaApantalla('0');
+});
+
 //Mas
-btnPress ='+'
-var btnMas = document.getElementById('mas')
-btnSiete.addEventListener('click',agregaApantalla)
+BtnMas = document.getElementById('mas');
+BtnMas.addEventListener('click', function(){ 
+    agregaApantalla('+');
+});
+
+//Menos
+BtnMenos = document.getElementById('menos');
+BtnMenos.addEventListener('click', function(){ 
+    agregaApantalla('-');
+});
+
+//Asterisco
+BtnAsterisco = document.getElementById('asterisco');
+BtnAsterisco.addEventListener('click', function(){ 
+    agregaApantalla('*');
+});
+
+//Parentesis Abre
+BtnParAbre = document.getElementById('parAbr');
+BtnParAbre.addEventListener('click', function(){ 
+    agregaApantalla('(');
+});
+
+//Parentesis que cierra.
+BtnParCierr = document.getElementById('parCierr');
+BtnParCierr.addEventListener('click', function(){ 
+    agregaApantalla(')');
+});
+
+//Igual
+BtnIgual = document.getElementById('igual');
+BtnIgual.addEventListener('click', function(){ 
+    agregaApantalla('=');
+});
+
+//Slash
+BtnSlash = document.getElementById('slash');
+BtnSlash.addEventListener('click', function(){ 
+    agregaApantalla('/');
+});
